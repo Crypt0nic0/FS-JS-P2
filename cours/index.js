a4a31e7475df98adfc9923854617cf66e1a35f2e
Crypt0nic0 <n.greneche45@gmail.com>
2023-03-10 11:14:58 +0100

    click event finished

--- cours/index.js
+++ cours/index.js
@@ -1,5 +1,32 @@
-// SELECTEURS
+//======================SELECTEURS============================
 // document.querySelector("h4").style.background="yellow";
 // const baliseHTML = document.querySelector("h4");
 
-// CLICK EVENT
+
+
+
+//======================CLICK EVENT===========================
+const questionContainer = document.querySelector('.click-event');
+const btn1 = document.querySelector('#btn-1');
+const btn2 = document.getElementById('btn-2');
+const response = document.querySelector('p');
+
+questionContainer.addEventListener('click', () => {
+    //questionContainer.classList.toggle('question-clicked');
+});
+
+btn1.addEventListener('click', () => {
+    response.classList.add('show-response');
+    response.style.color = 'green';
+});
+
+btn2.addEventListener('click', () => {
+    response.classList.add('show-response');
+    response.style.color = 'red';
+});
+
+
+
+
+
+//======================MOUSE EVENT===========================
