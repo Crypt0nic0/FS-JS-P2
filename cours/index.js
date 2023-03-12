78bb185ecef71f9b458919b4c233e15e0f9f0103
Crypt0nic0 <n.greneche45@gmail.com>
2023-03-12 21:09:37 +0100

    foreach syntax finished

--- cours/index.js
+++ cours/index.js
@@ -134,3 +134,12 @@ window.addEventListener("load", () => {
 });
 
 
+//-----------------------------------------------------------
+//================================================
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+    box.addEventListener('click', (e) => {
+        console.log(e.target);
+    });
+});
